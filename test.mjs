637a1ea5a3ec0e93f27486fd204eaf3d3bbf0b59
Jakub Robaczewski <kubarob13@gmail.com>
2025-06-10 15:35:14 +0200

    BUMP

--- test.mjs
+++ test.mjs
@@ -1,13 +1,39 @@
-let language = args.data.name.match(/\(([^)]+)\)/)?.[1]
-if (!language) {
-  language = "Reikspiel";
+let EFFECT = {
+  name: `${this.effect.name}: Powstrzymane`,
+  img: this.effect.img,
+  system: {
+    transferData: {},
+    scriptData: [
+      {
+        label: "Skrócenie Dystansu",
+        trigger: "prePrepareItem",
+        script: `
+              if (args.item.type == "weapon" && args.item.isEquipped)
+              {
+                let weaponLength = args.item.reachNum
+                if (weaponLength > 3)
+                {
+                  let improv = foundry.utils.duplicate(game.wfrp4e.config.systemItems.improv)
+                  improv.system.twohanded.value = args.item.twohanded.value
+                  improv.system.offhand.value = args.item.offhand.value
+                  improv.name = args.item.name + " (" + game.i18n.localize("EFFECT.Infighting") + ")"
+                  foundry.utils.mergeObject(args.item.system, improv.system, {overwrite : true})
+                  args.item.system.qualities = improv.system.qualities
+                  args.item.system.flaws = improv.system.flaws
+                  args.item.name = improv.name
+                  args.item.system.infighting = true;
+                }
+              }
+							`
+      }
+    ]
+  }
 }
-let skill = await game.wfrp4e.utility.findItem(`Język (${language})`, "skill")
-skill = foundry.utils.deepClone(skill)
-if (!skill) {
-  ui.notifications.error(`Nie znaleziono umiejętności 'Język (${language})'`);
+
+let test = await this.actor.setupSkill(game.i18n.localize("NAME.Cool"), {appendTitle: ` - ${this.effect.name}`, fields : {difficulty : "challenging"}})
+await test.roll();
+console.log(this)
+console.log(args)
+if (!test.failed) {
+  this.actor.createEmbeddedDocuments("ActiveEffect", [EFFECT], {fromEffect: this.item.id});
 }
-skill = skill.toObject();
-skill.system.advances.value = 10;
-skill.system.advances.force = true;
-this.actor.createEmbeddedDocuments("Item", [skill], {fromEffect : this.effect.id})
