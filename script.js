0e382e33e9a231e1b3c26083ca20da7d04bb2177
Jakub Robaczewski <kubarob13@gmail.com>
2025-09-13 10:56:57 +0200

    Bump

--- script.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs')
-
-let fileContent = fs.readFileSync(`report.txt`, 'utf-8').split('\r\n')
-
-fs.readdirSync('scripts').forEach(file => {
-  if (!fileContent.includes(file.split(".js")[0])) {
-    fs.unlinkSync(`scripts/${file}`);
-  }
-})
